cfb13aa13e10b7ca0ab76952659e66c2c3504434
Dylan Morgan <dbmorgan98@gmail.com>
2022-03-05 13:36:54 +0000

    Improved meshgrid generation Meshgrid now generates all grid points with even pacings

--- fe_mesh_gen.jl
+++ fe_mesh_gen.jl
@@ -1,5 +1,36 @@
 #!/usr/bin/env julia
 
+"""
+    pointsdiff(totpoints, xstart, eright, ystart, etop)
+
+Calculate the number of meshpoints required for each side of the shape
+"""
+function pointsdiff(totpoints, xstart, eright, ystart, etop)
+    # Generate a factor for difference of shape length
+    xdir = abs(eright - xstart)
+    ydir = abs(etop - ystart)
+    factor1 = xdir / ydir
+    factor2 = ydir / xdir
+
+
+    if factor1 > 1
+        factor = factor1
+        ypoints = round(sqrt(totpoints / factor))
+        xpoints = round(factor * ypoints)
+    else
+        factor = factor2
+        xpoints = round(sqrt(totpoints / factor))
+        ypoints = round(factor * xpoints)
+    end
+
+    return xpoints, ypoints
+end
+
+"""
+    elements(xpoints, ypoints, xstart, ystart, etop, eright)
+
+Create arrays of meshpoints for each side of the shape
+"""
 function elements(xpoints, ypoints, xstart, ystart, etop, eright)
     # x-direction
     bot = zeros(xpoints)
@@ -75,18 +106,39 @@ function mesh(bot, top, left, right)
     return xyz, con, dof
 end
 
+# TODO Split into modules in different files
+using Plots; gr()
 
-function main()
-    xpoints = 50; ypoints = 50
+function main(points=1000)
+    if points < 10
+        println(points, " grid point(s) have be specified.")
+        println("This can not run with less than 10 grid points!")
+        println("It is also recommended to use at least 100 grid points.")
+        throw(DomainError(points, "points argument must be ≥ 10"))
+    elseif points < 100
+        println("It is recommended to use at least 100 grid points.")
+        println(points, " points have been used here.")
+    end
 
-    # Split num of points by area of each part of shape
-    x1points = round(xpoints / 2)
-    x2points = xpoints - x1points
-    y1points = round(ypoints / 2)
-    y2points = ypoints - y1points
+    xstart1 = 0; xstart2 = 1.5
+    ystart1 = 4; ystart2 = 0
+    etop1 = 5; etop2 = 4
+    eright1 = 4; eright2 = 2.5
 
-    bot1, top1, left1, right1 = elements(Int(x1points), Int(y1points), 0, 4, 5, 4)
-    bot2, top2, left2, right2 = elements(Int(x2points), Int(y2points), 1.5, 0, 4, 2.5)
+    # Assign points to each solid element
+    x1points, y1points = pointsdiff(points, xstart1, eright1, ystart1, etop1)
+    x2points, y2points = pointsdiff(points, xstart2, eright2, ystart2, etop2)
+
+    # Remove top row from lower element
+    y2range = LinRange(xstart2, eright2, Int(x2points))
+    reinterpret(Float64, y2range)
+    y2pen = y2range[end] - y2range[end - 1]
+
+    # Obtain mesh arrays for solid element sides
+    bot1, top1, left1, right1 = elements(Int(x1points), Int(y1points), xstart1,
+                                         ystart1, etop1, eright1)
+    bot2, top2, left2, right2 = elements(Int(x2points), Int(y2points), xstart2,
+                                         ystart2, etop2 - y2pen, eright2)
 
     xyz1, con1, dof1 = mesh(bot1, top1, left1, right1)
     xyz2, con2, dof2 = mesh(bot2, top2, left2, right2)
@@ -95,15 +147,21 @@ function main()
     meshgrid = plot(
         xyz1[:,1], xyz1[:,2],
         seriestype = :scatter,
+        aspect_ratio = :equal,
+        xlim = (-0.2, 4.2),
+        legend = :none,
+        markersize = 1, markershape = :circle, markercolor = :black,
         title = "Meshgrid"
     )
 
     plot!(meshgrid,
           xyz2[:,1], xyz2[:,2],
-          seriestype = :scatter
+          seriestype = :scatter,
+          markersize = 1, markershape = :circle, markercolor = :black
           )
 
     display(meshgrid)
 end
 
+# TODO Parse arguments from the REPL
 main()
